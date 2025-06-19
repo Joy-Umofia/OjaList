f245b649118b9f937b2c98e964e73b96e284b858
joy umofia <joyumofia@gmail.com>
2025-06-19 11:29:06 +0100

    made the delete icon functional

--- grocery-list/src/App.jsx
+++ grocery-list/src/App.jsx
@@ -13,11 +13,31 @@ import './App.css'
 
 function App() {
   const[items,setItems]=useState([])
+   const[description,setDescription]=useState("")
+  const[price,setPrice]=useState("")
+  const[editId,setEditId]=useState(null)
+
+  // deleting items
+  function handleDeleteItems(id){
+    setItems(items=>items.filter((item)=>item.id !==id))
+  }
+  // editing the items
+  function editItems(id) {
+    console.log("edit icon clicked")
+  const itemToEdit = items.find(item => item.id === id);
+  if (itemToEdit) {
+    setDescription(itemToEdit.description);
+    setPrice(itemToEdit.price.toString());
+    setEditId(id) 
+  }
+}
   return(
     <div>
       <Logo />
       <Budget />
-      <Item addItems={items} upDateItems={setItems}/>
+      <Item addItems={items} upDateItems={setItems}
+      deleteItems={handleDeleteItems} description={description} setDescription={setDescription} price={price} setPrice={setPrice} editItems={editItems} editId={editId}
+      setEditId={setEditId}/>
       
     </div>
   )
@@ -45,25 +65,43 @@ function Budget(){
   )
 }
 
-function Item({addItems,upDateItems}){
-  const[description,setDescription]=useState("")
-  const[price,setPrice]=useState("")
+function Item({addItems,upDateItems,deleteItems,description,setDescription,price,setPrice,editItems,editId,setEditId}){
+  // const[description,setDescription]=useState("")
+  // const[price,setPrice]=useState("")
   
   
   
  function handleSubmit(e){
      e.preventDefault()
-     if(!description)alert("Please input an item")
-    const newItem={
-    id:Date.now(),
-    description:description,
-    price:Number(price),
-    packed:false,
+     if(!description) {
+      alert("You forgot to input an item")
+      return
+     }
+    if(!price) {
+      alert("Input your price so you dont't over spend")
+      return
+    }
+    
+    
+    const newItem = {
+      id: Date.now(),
+      description,
+      price: Number(price),
+      packed: false,
+    };
+    upDateItems([...addItems, newItem]);
   }
-   upDateItems([...addItems,newItem])
+    
+  //   const newItem={
+  //   id:Date.now(),
+  //   description:description,
+  //   price:Number(price),
+  //   packed:false,
+  // }
+  //  upDateItems([...addItems,newItem])
 
-      setDescription("")
-      setPrice("")
+    setDescription("")
+    setPrice("")
   }
   return(
     <div className='item-section'>
@@ -72,33 +110,34 @@ function Item({addItems,upDateItems}){
         <div className="items-input">
           <input type="text" placeholder='item description' value={description}onChange={(e)=>setDescription(e.target.value)}/>
           <input type="number" placeholder='price' value={price} onChange={(e)=>setPrice(e.target.value)}/>
-          <button> + Add Item</button>
+          <button>{editId !== null ? " Update" : "+ Add Item"}</button>
         </div>
       </form>
-      <CardSection addItems={addItems} upDateItems={upDateItems}/>
+      <CardSection addItems={addItems} upDateItems={upDateItems} deleteItems={deleteItems} editItems={editItems} editId={editId}/>
       <Statistics />
     </div>
   )
 }
 
-function CardSection({addItems}){
+function CardSection({addItems,deleteItems,editItems}){
   return(
     <div className='items'>
-      {addItems.map((item)=><Cards item={item} key={item.id} />)}
+      {addItems.map((item)=><Cards item={item} key={item.id} deleteItems={deleteItems} editItems={editItems}/>)}
     </div>
   )
 }
 
-function Cards({item}){
+function Cards({item,deleteItems,editItems}){
    return(
     <div className='items-card'>
-         <div>
+        <div>
           <input type="checkbox" name="" id="" />
           <p style={item.packed?{textDecoration:"line-through"}:{}}>{item.description}</p>
         </div>
         <div>
           <p>{`₦${item.price}`}</p>
-          <span>🗑️</span>
+          <span style={{cursor:"pointer"}} onClick={()=>deleteItems(item.id)} title="delete">🗑️</span>
+          <span style={{cursor:"pointer"}} onClick={()=>editItems(item.id)} title="edit">✏️</span>
         </div>
       </div>
    )
